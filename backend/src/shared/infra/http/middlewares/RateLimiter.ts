16af54ceb6f014a2137f279d19ec93699c783627
otoniricardo <otoniricardo@gmail.com>
2020-08-29 14:56:04 -0300

    finaliza backend

--- backend/src/shared/infra/http/middlewares/RateLimiter.ts
+++ backend/src/shared/infra/http/middlewares/RateLimiter.ts
@@ -1,32 +1,32 @@
 /* eslint-disable import/no-extraneous-dependencies */
-// import { Request, Response, NextFunction } from 'express';
-// import { RateLimiterRedis } from 'rate-limiter-flexible';
-// import redis from 'redis';
+import { Request, Response, NextFunction } from 'express';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import redis from 'redis';
 
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
-// const redisClient = redis.createClient({
-//   host: process.env.REDIS_HOST,
-//   port: Number(process.env.REDIS_PORT),
-//   password: process.env.REDIS_PASS || undefined,
-// });
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASS || undefined,
+});
 
-// const limiter = new RateLimiterRedis({
-//   storeClient: redisClient,
-//   keyPrefix: 'rateLimit',
-//   points: 5,
-//   duration: 1,
-// });
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'rateLimit',
+  points: 5,
+  duration: 1,
+});
 
-// export default async function rateLimiter(
-//   request: Request,
-//   response: Response,
-//   next: NextFunction,
-// ): Promise<void> {
-//   try {
-//     await limiter.consume(request.ip);
-//     return next();
-//   } catch (err) {
-//     throw new AppError('Too many requests', 429);
-//   }
-// }
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    await limiter.consume(request.ip);
+    return next();
+  } catch (err) {
+    throw new AppError('Too many requests', 429);
+  }
+}
